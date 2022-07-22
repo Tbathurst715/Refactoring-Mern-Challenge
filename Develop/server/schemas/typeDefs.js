7a320f825412ec50f07e527552fb83c37bb66860
Tbathurst715 <timbathurst@yahoo.com>
2022-07-22 13:56:14 -0600

    Created typeDefs

---
+++ Develop/server/schemas/typeDefs.js
@@ -0,0 +1,39 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    authors: String
+    bookId: ID!
+    description: String
+    image: String
+    title: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    user(userId: ID!): User
+    books: [Book]!
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(userId: ID!, savedBooks: String!): User
+    removeBook(bookId: String!): User
+  }
+  ;
+
+module.exports = typeDefs;
